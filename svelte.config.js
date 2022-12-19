5061eb5cb89e15c0519d58b32c34703e97ae6509
Sumanth Chinthagunta <xmlking@gmail.com>
2022-12-19 14:39:56 -0800

    chore(root): first commit

---
+++ svelte.config.js
@@ -0,0 +1,47 @@
+import adapter from '@sveltejs/adapter-auto';
+import nodeAdapter from '@sveltejs/adapter-node';
+import staticAdapter from '@sveltejs/adapter-static';
+import { vitePreprocess } from '@sveltejs/kit/vite';
+import { construct_svelte_component } from 'svelte/internal';
+
+/** @type {import('@sveltejs/kit').Config} */
+const config = {
+	// Consult https://kit.svelte.dev/docs/integrations#preprocessors
+	// for more information about preprocessors
+	preprocess: vitePreprocess(),
+
+	kit: {
+		adapter: getAdapter(),
+
+		// When hosting SPA on GitHub Pages
+		// paths: {
+		// 	base: dev ? '' : '/svelte-starter-kit',
+		// },
+
+		// prerender: { entries: [] },
+		alias: {
+			$mocks: 'src/mocks',
+			$houdini: './$houdini'
+		}
+	}
+};
+
+export default config;
+
+function getAdapter() {
+	switch (process.env.ADAPTER) {
+		case 'node':
+			return nodeAdapter({
+				// envPrefix: 'CONFY_'
+				// Can overwrite at runtime: `HOST=127.0.0.1 PORT=4000 ORIGIN=https://my.site node build`
+				// default: HOST=0.0.0.0 PORT=3000
+			});
+		case 'static':
+			return staticAdapter({
+				fallback: 'index.html' // 404.html,
+				// trailingSlash: 'always'
+			});
+		default:
+			return adapter();
+	}
+}
