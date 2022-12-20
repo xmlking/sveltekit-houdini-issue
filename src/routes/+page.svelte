cce08e651fb4e48c5d9f12fbb5abac0c77f14f58
Sumanth Chinthagunta <xmlking@gmail.com>
2022-12-20 09:39:53 -0800

    build(deps): updated houdini to 0.19.0

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,3 +1,42 @@
+<script lang="ts">
+	import { CachePolicy, graphql } from '$houdini';
+
+	const allOrders = graphql(`
+		query allOrders($limit: Int = 10) {
+			customer(limit: $limit) @paginate {
+				email
+				first_name
+				id
+				ip_address
+				last_name
+				phone
+				username
+				orders {
+					customer_id
+					discount_price
+					id
+					order_date
+					product
+					purchase_price
+					transaction_id
+				}
+			}
+		}
+	`);
+
+	$: console.log('$allOrders.data on load:', $allOrders.data);
+</script>
+
 <h1>Welcome to SvelteKit</h1>
 <p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
-<a href="#" class="btn-primary btn">btn</a>
+
+<br />
+<button class="btn" on:click={() => allOrders.loadNextPage()}>load more</button>
+<button class="btn-accent btn" on:click={() => allOrders.fetch({ policy: CachePolicy.NetworkOnly })}>refetch</button>
+<br />
+<span>isFetching: {$allOrders.isFetching}</span>
+{#each $allOrders.data.customer as customer}
+	<p> Customer: {customer.first_name} {customer.last_name} <span>Order Count:  {customer.orders.length}</span></p>
+	<!-- <pre>{JSON.stringify(customer, null, 2)}</pre> -->
+{/each}
+
---
+++ src/routes/+page.ts
@@ -0,0 +1,5 @@
+export function _allOrdersVariables() {
+	return {
+		limit: 2
+	};
+}
